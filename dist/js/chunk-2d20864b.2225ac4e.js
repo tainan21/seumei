3e1ba91954d2466215009fd0941427e1ac81efb1
Tainan Camargo de Moraes <tainancmoraes@gmail.com>
2022-06-04 18:14:08 -0300

    alter teste manutence

--- dist/js/chunk-2d20864b.2225ac4e.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20864b"],{a534:function(t,e,n){"use strict";n.r(e);var c=n("7a23"),i={class:"flex align-items-center py-5 px-3"},a=Object(c["j"])("i",{class:"pi pi-fw pi-ticket mr-2 text-2xl"},null,-1),s=Object(c["j"])("p",{class:"m-0 text-lg"},"Seat Component Content via Child Route",-1),p=[a,s];function l(t,e,n,a,s,l){return Object(c["E"])(),Object(c["i"])("div",i,p)}var o={},r=n("6b0d"),u=n.n(r);const b=u()(o,[["render",l]]);e["default"]=b}}]);
-//# sourceMappingURL=chunk-2d20864b.2225ac4e.js.map
