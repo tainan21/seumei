d2e130ca1cc5743b05e15e55ef810d56f1e6c5e7
Tainan Camargo de Moraes <tainancmoraes@gmail.com>
2022-06-04 19:01:48 -0300

    manutence backup alter

---
+++ dist/js/chunk-2d20864b.a0dd59b6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d20864b"],{a534:function(t,e,n){"use strict";n.r(e);var c=n("7a23"),i={class:"flex align-items-center py-5 px-3"},a=Object(c["j"])("i",{class:"pi pi-fw pi-ticket mr-2 text-2xl"},null,-1),s=Object(c["j"])("p",{class:"m-0 text-lg"},"Seat Component Content via Child Route",-1),p=[a,s];function l(t,e,n,a,s,l){return Object(c["E"])(),Object(c["i"])("div",i,p)}var o={},r=n("d959"),u=n.n(r);const d=u()(o,[["render",l]]);e["default"]=d}}]);
+//# sourceMappingURL=chunk-2d20864b.a0dd59b6.js.map
